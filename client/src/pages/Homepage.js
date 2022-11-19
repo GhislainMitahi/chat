432ac62b55ac3634730f0f6f9b1cd288fb7949ba
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-19 19:55:48 +0200

    update home page

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text
+} from '@chakra-ui/react';
 
 const Homepage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW='xl' centerContent>
+      <Box
+         d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+           <Text fontSize='4xl' fontFamily="work sans" color='black'>Talk-A-Tive</Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant='soft-rounded' colorScheme='green'>
+  <TabList mb="1rem">
+    <Tab width="50%">Login</Tab>
+    <Tab width="50%">Sing up</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+      </Box>
+    </Container>
   )
 }
 
