cceb3a90e2bf4aeb5e18ddecb73a247ffdf181e8
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-23 09:56:59 +0200

    add chat model

---
+++ server/models/chatModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const chatModel = mongoose.Schema(
+    {
+        chatName: { type: 'string', trim: true },
+        isGroupChat: { type: Boolean, default: false },
+        users: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message",
+        },
+        groupeAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.export = Chat
+// In this chatModel file we will have model of:
+       // ChatName 
+       //isGroupChat 
+       //users
+       //groupeAdmin
+
+
