5630a113bea8412666d308c8140ac5ea6ab2226e
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-23 09:22:59 +0200

    add token generator

---
+++ server/config/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, "ghislain", {
+        expiresIn: "30d",
+    })
+}
+
+module.exports = generateToken;
