fab13cdd54318c9a69344aee7c89fcdda2bfa727
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-25 03:19:15 +0200

    update signup

--- client/src/components/Authentification/Signup.js
+++ client/src/components/Authentification/Signup.js
@@ -43,7 +43,7 @@ const Signup = () => {
       });
       return;
     }
-    console.log(name, email, password, pic);
+   console.log(name, email, password, pic);
     try {
       const config = {
         headers: {
@@ -100,9 +100,9 @@ const Signup = () => {
     if (pics.type === "image/jpeg" || pics.type === "image/png") {
       const data = new FormData();
       data.append("file", pics);
-      data.append("upload_preset", "chat-app");
-      data.append("cloud_name", "piyushproj");
-      fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
+      data.append("upload_preset", "chat-Pic");
+      data.append("cloud_name", "ghislainmitahi");
+      fetch("https://api.cloudinary.com/v1_1/ghislainmitahi/image/upload", {
         method: "post",
         body: data,
       })
