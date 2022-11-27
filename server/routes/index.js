8bc7f048d7c400a40b978bdef1c34045e7c3378f
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-27 10:18:24 +0200

    update index router

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 const chats = require('../data/data.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/g', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/chat', function(req, res, next) {
+router.get('/api/chatt', function(req, res, next) {
   res.send(chats);
 }); 
 
-router.get('/api/chat/:id', function(req, res, next) {
+router.get('/api/chatt/:id', function(req, res, next) {
   // console.log(req.params.id);
 
   const peerToPeerChat = chats.find((c) => c._id === req.params.id);
