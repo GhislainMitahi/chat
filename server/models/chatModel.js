27abb227b18ec5b2ddc23c3a739a4b0e507ba7a5
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-27 11:04:23 +0200

    update chat model

--- server/models/chatModel.js
+++ server/models/chatModel.js
@@ -14,7 +14,7 @@ const chatModel = mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: "Message",
         },
-        groupeAdmin: {
+        groupAdmin: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
         },
