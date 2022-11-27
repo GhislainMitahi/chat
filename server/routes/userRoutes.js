fd6a7e0ad3b87c3e618a4507f84eb02344adfd3b
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-11-27 10:19:12 +0200

    update userROuter fine name

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const { registerUser, authUser, allUsers } = require('../controllers/userControllers');
+const { protect } = require("../middlewares/authMiddleware")
+
+router.route('/').post(registerUser).get(protect, allUsers);
+router.post('/login', authUser);
+
+
+module.exports = router;
