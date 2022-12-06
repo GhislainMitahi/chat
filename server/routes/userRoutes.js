1b61af3e6c276229753497089cb6fef611a96727
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-12-06 15:11:19 +0200

    add userRouter

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { registerUser, authUser, allUsers } = require('../controllers/userControllers');
-const { protect } = require("../middlewares/authMiddleware")
+const { protect } = require("../middlewares/authMiddleware");
 
 router.route('/').post(registerUser).get(protect, allUsers);
 router.post('/login', authUser);
